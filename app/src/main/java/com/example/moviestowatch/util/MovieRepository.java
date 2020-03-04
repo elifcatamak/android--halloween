47937bebca87b77479e28f9f7fd96bb22e1b8100
Elif Ozer <elifozer173@gmail.com>
2020-03-04 18:32:41 +0300

    Implemented insert and getAll methods in MovieRepository Added a view model for Movie

--- app/src/main/java/com/example/moviestowatch/util/MovieRepository.java
+++ app/src/main/java/com/example/moviestowatch/util/MovieRepository.java
@@ -1,4 +1,47 @@
 package com.example.moviestowatch.util;
 
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import com.example.moviestowatch.data.MovieDao;
+import com.example.moviestowatch.data.MovieRoomDb;
+import com.example.moviestowatch.model.Movie;
+
+import java.util.List;
+
 public class MovieRepository {
+    private MovieDao movieDao;
+    private LiveData<List<Movie>> movieList;
+
+    public MovieRepository(Application application) {
+        MovieRoomDb db = MovieRoomDb.getDatabase(application);
+
+        this.movieDao = db.getMovieDao();
+        this.movieList = movieDao.getAllMovies();
+    }
+
+    public LiveData<List<Movie>> getMovieList(){
+        return movieList;
+    }
+
+    public void insertMovie(Movie movie){
+        new insertMovieAsync(movieDao).execute(movie);
+    }
+
+    private static class insertMovieAsync extends AsyncTask<Movie, Void, Void> {
+        MovieDao dao;
+
+        public insertMovieAsync(MovieDao movieDao) {
+            this.dao = movieDao;
+        }
+
+        @Override
+        protected Void doInBackground(Movie... movies) {
+            dao.insertMovie(movies[0]);
+
+            return null;
+        }
+    }
 }
---
+++ app/src/main/java/com/example/moviestowatch/util/MovieViewModel.java
@@ -0,0 +1,31 @@
+package com.example.moviestowatch.util;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.moviestowatch.model.Movie;
+
+import java.util.List;
+
+public class MovieViewModel extends AndroidViewModel {
+    private MovieRepository movieRepository;
+    private LiveData<List<Movie>> moveList;
+
+    public MovieViewModel(@NonNull Application application) {
+        super(application);
+
+        movieRepository = new MovieRepository(application);
+        this.moveList = movieRepository.getMovieList();
+    }
+
+    public LiveData<List<Movie>> getMoveList(){
+        return moveList;
+    }
+
+    public void insertMovie(Movie movie){
+        movieRepository.insertMovie(movie);
+    }
+}
